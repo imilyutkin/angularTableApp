4a369dafa2ae15027f8716dfb41c7d4afd749639
Ivan, Milyutkin <ivan.milyutkin@gmail.com>
2015-09-07 12:06:20 +0300

    git init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTableApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularTableApp
+ */
+angular.module('angularTableApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTableApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTableApp
+ */
+angular.module('angularTableApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
